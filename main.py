55f6d13228b2940ec5df384ff1579836179e1e9a
M04128012 <a589658gur@gmail.com>
2020-03-16 23:17:45 +0800

    test connect db

--- main.py
+++ main.py
@@ -41,7 +41,7 @@ def login():
             response=f'RRR {e} , {aa}'
         )
     if request.method == 'GET':
-        return render_template('login.html', data=aa)
+        return render_template('login.html', data=aa[0])
 
     return render_template('login.html')
 
