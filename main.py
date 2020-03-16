f2b36cae941c77f450dc71489457a9d81cfc9363
M04128012 <a589658gur@gmail.com>
2020-03-16 22:24:53 +0800

    連接google cloud

--- main.py
+++ main.py
@@ -1,14 +1,37 @@
 import os
 
 
-from flask import Flask, render_template
+from flask import Flask, render_template, request
+import sqlalchemy
+
+db_user = os.environ.get("DB_USER")
+db_pass = os.environ.get("DB_PASS")
+db_name = os.environ.get("DB_NAME")
+cloud_sql_connection_name = os.environ.get("CLOUD_SQL_CONNECTION_NAME")
 
 app = Flask(__name__)
 
+db = sqlalchemy.create_engine(
+    sqlalchemy.engine.url.URL(
+        drivername="mysql+pymysql",
+        username=db_user,
+        password=db_pass,
+        database=db_name,
+        query={"unix_socket": "/cloudsql/{}".format(cloud_sql_connection_name)},
+    ),
+    pool_size=5,
+    max_overflow=2,
+    pool_timeout=30,
+    pool_recycle=1800,
+)
 
-@app.route('/')
-def hello_world():
-    return render_template('index.html')
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    if request.method == 'GET':
+        return render_template('login.html')
+
+    return render_template('login.html')
 
 
 if __name__ == '__main__':
