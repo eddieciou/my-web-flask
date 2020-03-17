fbc73d3bf5968acc3d20e45a6ba79171087c045b
M04128012 <a589658gur@gmail.com>
2020-03-17 13:58:55 +0800

    使用flask-sqlalchemy

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import os
 
 from flask import Flask, render_template, request, Response
-import sqlalchemy
+from flask_sqlalchemy import SQLAlchemy
 
 
 db_user = os.environ.get("DB_USER")
@@ -9,31 +9,40 @@ db_pass = os.environ.get("DB_PASS")
 db_name = os.environ.get("DB_NAME")
 cloud_sql_connection_name = os.environ.get("CLOUD_SQL_CONNECTION_NAME")
 
+db = SQLAlchemy()
+
 app = Flask(__name__)
 
-db = sqlalchemy.create_engine(
-    sqlalchemy.engine.url.URL(
-        drivername="mysql+pymysql",
-        username=db_user,
-        password=db_pass,
-        database=db_name,
-        query={"unix_socket": "/cloudsql/{}".format(cloud_sql_connection_name)},
-    ),
-    pool_size=5,
-    max_overflow=2,
-    pool_timeout=30,
-    pool_recycle=1800,
-)
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config['SQLALCHEMY_DATABASE_URI'] = f'mysql+mysqldb://{db_user}:{db_pass}@/{db_name}' \
+    f'?unix_socket=/cloudsql/{cloud_sql_connection_name}'
+
+db.init_app(app)
+
+
+# db = sqlalchemy.create_engine(
+#     sqlalchemy.engine.url.URL(
+#         drivername="mysql+pymysql",
+#         username=db_user,
+#         password=db_pass,
+#         database=db_name,
+#         query={"unix_socket": "/cloudsql/{}".format(cloud_sql_connection_name)},
+#     ),
+#     pool_size=5,
+#     max_overflow=2,
+#     pool_timeout=30,
+#     pool_recycle=1800,
+# )
 
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
-    stmt = sqlalchemy.text(
-        "SELECT * FROM users"
-    )
+    sql_cmd = """
+        select *
+        from users
+        """
     try:
-        with db.connect() as coon:
-            aa = coon.execute(stmt).fetchone()
+        aa = db.engine.execute(sql_cmd)
     except Exception as e:
         return Response(
             status=500,
@@ -41,7 +50,7 @@ def login():
         )
     if request.method == 'GET':
         try:
-            return render_template('login.html', data=aa[0])
+            return render_template('login.html', data=aa)
         except Exception as e:
             return Response(
                 status=500,
