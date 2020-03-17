fdb19bcf34b8bf9d9e537230f4b0119422972fe0
M04128012 <a589658gur@gmail.com>
2020-03-17 21:51:30 +0800

    test user

--- app/modules/auth/resources.py
+++ app/modules/auth/resources.py
@@ -5,5 +5,10 @@ from .models import User
 
 class Login(Resource):
     def get(self):
-        data = User.query.filter_by(username='eddieciou').first()
-        return make_response(render_template('login.html', data=data))
+        try:
+            data = User.query.filter_by(username='eddieciou').first()
+            return make_response(render_template('login.html', data=data))
+        except Exception as e:
+            return {
+                "error": f'{e}'
+            }
