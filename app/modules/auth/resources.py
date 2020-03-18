876112877884775aa363938aec1a2fd6f7e2c4db
M04128012 <a589658gur@gmail.com>
2020-03-18 09:58:12 +0800

    test user

--- app/modules/auth/resources.py
+++ app/modules/auth/resources.py
@@ -1,15 +1,15 @@
 from flask_restful import Resource
 from flask import render_template, make_response
-# from .models import User
+from .models import User
 
 
 class Login(Resource):
     def get(self):
-        # try:
-        #     # data = User.query.filter_by(username='eddieciou').first()
-        #     return make_response(render_template('login.html', data=data))
-        # except Exception as e:
-        #     return {
-        #         "error": f'{e}'
-        #     }
-        return make_response(render_template('login.html', data="dddd"))
+        try:
+            data = User.query.filter_by(username='eddieciou').first()
+            return make_response(render_template('login.html', data=data))
+        except Exception as e:
+            return {
+                "error": f'{e}'
+            }
+        # return make_response(render_template('login.html', data="dddd"))
